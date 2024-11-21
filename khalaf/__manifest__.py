3d7bce6f4417a6067a771f6c867fc15d7b753535
Mahmoud Abdel Latif <mabdellatif@tanmeyah.com>
2024-11-21 23:13:11 +0200

    delivery boy

--- khalaf/__manifest__.py
+++ khalaf/__manifest__.py
@@ -25,6 +25,7 @@ This module enhances the operational efficiency of grocery delivery services, en
     'depends': ['stock', 'hr','sale'],
     'data': [
         'security/ir.model.access.csv',
+        'views/stock_picking_receipt_report.xml',
         'views/delivery_boy_views.xml',
         'views/sale_order.xml',
         'views/assign_delivery_boy_wizard_views.xml',
